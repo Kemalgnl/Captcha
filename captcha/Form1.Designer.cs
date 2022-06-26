1be4d1db70c9b0c17a0c9bb46c3ed28939a8c1d8
KEMAL <KEMAL@CASPER>
2022-06-26 18:36:50 +0300

    Proje dosyası ekle.

---
+++ captcha/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace captcha
+{
+	partial class captch
+	{
+		/// <summary>
+		///Gerekli tasarımcı değişkeni.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///Kullanılan tüm kaynakları temizleyin.
+		/// </summary>
+		///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer üretilen kod
+
+		/// <summary>
+		/// Tasarımcı desteği için gerekli metot - bu metodun 
+		///içeriğini kod düzenleyici ile değiştirmeyin.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(captch));
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button1.Location = new System.Drawing.Point(753, 52);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(180, 45);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Create";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.SystemColors.GrayText;
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.textBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.textBox1.Location = new System.Drawing.Point(352, 53);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(71, 50);
+			this.textBox1.TabIndex = 3;
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.button2.Location = new System.Drawing.Point(753, 142);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(180, 45);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "Control";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.BackColor = System.Drawing.SystemColors.GrayText;
+			this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.textBox2.ForeColor = System.Drawing.SystemColors.Window;
+			this.textBox2.Location = new System.Drawing.Point(442, 53);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(71, 50);
+			this.textBox2.TabIndex = 10;
+			// 
+			// textBox3
+			// 
+			this.textBox3.BackColor = System.Drawing.SystemColors.GrayText;
+			this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.textBox3.ForeColor = System.Drawing.SystemColors.Window;
+			this.textBox3.Location = new System.Drawing.Point(529, 53);
+			this.textBox3.Multiline = true;
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(71, 50);
+			this.textBox3.TabIndex = 12;
+			// 
+			// textBox4
+			// 
+			this.textBox4.BackColor = System.Drawing.SystemColors.GrayText;
+			this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.textBox4.ForeColor = System.Drawing.SystemColors.Window;
+			this.textBox4.Location = new System.Drawing.Point(619, 53);
+			this.textBox4.Multiline = true;
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(71, 50);
+			this.textBox4.TabIndex = 11;
+			// 
+			// textBox5
+			// 
+			this.textBox5.BackColor = System.Drawing.SystemColors.GrayText;
+			this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.textBox5.ForeColor = System.Drawing.SystemColors.Window;
+			this.textBox5.Location = new System.Drawing.Point(352, 137);
+			this.textBox5.Multiline = true;
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(71, 50);
+			this.textBox5.TabIndex = 16;
+			// 
+			// textBox6
+			// 
+			this.textBox6.BackColor = System.Drawing.SystemColors.GrayText;
+			this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.textBox6.ForeColor = System.Drawing.SystemColors.Window;
+			this.textBox6.Location = new System.Drawing.Point(442, 137);
+			this.textBox6.Multiline = true;
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(71, 50);
+			this.textBox6.TabIndex = 15;
+			// 
+			// textBox7
+			// 
+			this.textBox7.BackColor = System.Drawing.SystemColors.GrayText;
+			this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.textBox7.ForeColor = System.Drawing.SystemColors.Window;
+			this.textBox7.Location = new System.Drawing.Point(529, 136);
+			this.textBox7.Multiline = true;
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(71, 50);
+			this.textBox7.TabIndex = 14;
+			// 
+			// textBox8
+			// 
+			this.textBox8.BackColor = System.Drawing.SystemColors.GrayText;
+			this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.textBox8.ForeColor = System.Drawing.SystemColors.Window;
+			this.textBox8.Location = new System.Drawing.Point(619, 136);
+			this.textBox8.Multiline = true;
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(71, 50);
+			this.textBox8.TabIndex = 13;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label3.Location = new System.Drawing.Point(86, 59);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(248, 32);
+			this.label3.TabIndex = 17;
+			this.label3.Text = "Security Keywords";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+			this.label4.Location = new System.Drawing.Point(86, 149);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(251, 32);
+			this.label4.TabIndex = 18;
+			this.label4.Text = "The words you see";
+			// 
+			// captch
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.AutoSize = true;
+			this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+			this.ClientSize = new System.Drawing.Size(971, 261);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.textBox8);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "captch";
+			this.Text = "Form1";
+			this.TransparencyKey = System.Drawing.Color.Red;
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+	}
+}
+
---
+++ captcha/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace captcha
+{
+	public partial class captch : Form
+	{
+		public captch()
+		{
+			InitializeComponent();
+		}
+
+		private void textBox6_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			char[] MyArray = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'İ', 'J', 'K', 'L', 'M', 'N', 'O', 'Ö', 'P', 'R', 'S', 'Ş', 'T', 'U', 'Ü', 'V', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+			Random x = new Random();
+
+				int random1 = x.Next(0, MyArray.Length);
+				textBox1.Text= Convert.ToString(MyArray[random1]);
+
+				int random2 = x.Next(0, MyArray.Length);
+				textBox2.Text = Convert.ToString(MyArray[random2]);
+
+				int random3 = x.Next(0, MyArray.Length);
+				textBox3.Text = Convert.ToString(MyArray[random3]);
+
+				int random4 = x.Next(0, MyArray.Length);
+				textBox4.Text = Convert.ToString(MyArray[random4]);
+	
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (textBox5.Text.ToUpper() == textBox1.Text && textBox6.Text.ToUpper() == textBox2.Text &&
+				textBox7.Text.ToUpper() == textBox3.Text && textBox8.Text.ToUpper() == textBox4.Text)
+				MessageBox.Show("Login successful");
+			else
+			{
+				MessageBox.Show("you made a mistake");
+			}
+		}
+
+		private void textBox5_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
